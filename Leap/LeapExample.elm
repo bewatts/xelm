d6987697c7b087a15c65c761db51d19a4564fd62
Benjamin Watts <ben@benjaminclaywatts.com>
2015-09-08 21:30:31 -0700

    Add first problem, Leap, based off xruby
    Note that the inclusion of the example is
    temporary, and should be taken out once CI
    is up and running.

---
+++ Leap/LeapExample.elm
@@ -0,0 +1,6 @@
+module LeapExample where
+
+isLeap : Int -> Bool
+isLeap year =
+  let divisible_by i = year `rem` i == 0
+  in (divisible_by 400) || (divisible_by 4) &&  not (divisible_by 100)
---
+++ Leap/LeapTest.elm
@@ -0,0 +1,19 @@
+module LeapTest where
+
+-- TODO - remove example inclusion once Problem sets are ready to go live or CI is set up.
+
+import LeapExample exposing (isLeap)
+import ElmTest.Test exposing (test, Test, suite)
+import ElmTest.Assertion exposing (assert, assertEqual)
+import ElmTest.Runner.Element exposing (runDisplay)
+
+tests : Test
+tests = suite "Leap Test Suite"
+        [ test "Yes, 1996 is a leap year" (assertEqual (isLeap 1996) True),
+          test "No, 1997 is a leap year" (assertEqual  (isLeap 1997) False),
+          test "No, 1998 is a leap year" (assertEqual  (isLeap 1998) False),
+          test "No, 1900 is a leap year" (assertEqual  (isLeap 1900) False),
+          test "Yes, 2400 is a leap year" (assertEqual (isLeap 2400) True)
+        ]
+
+main = runDisplay tests
