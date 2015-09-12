b2e8c81971545a669209c5e9a3031a95b50cdaf1
Benjamin Watts <ben@benjaminclaywatts.com>
2015-09-12 13:38:29 -0700

    Accumulate

---
+++ Accumulate/AccumulateTest.elm
@@ -0,0 +1,22 @@
+module AccumulateTest where
+
+-- TODO - remove example inclusion once Problem sets are ready to go live or CI is set up.
+
+import AccumulateExample exposing (accumulate)
+import ElmTest.Test exposing (test, Test, suite)
+import ElmTest.Assertion exposing (assert, assertEqual)
+import ElmTest.Runner.Element exposing (runDisplay)
+import String
+
+square : Int -> Int
+square x = x * x
+
+tests : Test
+tests = suite "Accumulate"
+        [ test "[]] Accumulate" (assertEqual (accumulate square []) []),
+          test "square Accumulate" (assertEqual (accumulate square [1,2,3]) [1,4,9]),
+          test "toUpper Accumulate" (assertEqual (accumulate String.toUpper ["hello" , "world"]) ["HELLO","WORLD"]),
+          test "reverse Accumulate" (assertEqual (accumulate String.reverse ["hello" , "world"]) ["olleh","dlrow"])
+        ]
+        
+main = runDisplay tests
